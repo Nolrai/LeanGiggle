0f5ba43ebb99b246ef604ade506075f25b27b974
Chris Upshaw <chrisaupshaw.work@gmail.com>
2018-05-05 18:03:09 -0700

    Stuff

--- Preorders.lean
+++ Preorders.lean
@@ -1,34 +1,45 @@
 universe u
 
-def vacuous {T:Sort u} (e : empty) : T := 
-match e with
-end
-
 def one : preorder unit :=
 { le := λ _ _, true
 , le_refl := by intro; split
 , le_trans := by intros; split}
 
-def set_inclusion (T : Type) : preorder (set T) :=
-{ preorder . O := set T
-, R := set.subset 
-, id := λ _ _ H, H
-, comp := λ _ _ _ f g _ inA, f (g inA)
-}
-
 section
 variable T : Type
 
-structure partition := (box : T -> set T) (correct : ∀ t s, t ∈ box s <-> box t = box s)
+def inclusion_trans 
+(a b c : set T) 
+(AB : a ⊆ b) 
+(BC : b ⊆ c)
+: a ⊆ c := λ t tInA, BC (AB tInA)
+
+instance set_inclusion : preorder (set T) :=
+{ preorder . le := set.subset 
+, le_refl := λ A t inA, inA
+, le_trans := inclusion_trans T
+}
+
+structure partition := 
+    (box : T -> set T)
+    (correct : ∀ t s, t ∈ box s <-> box t = box s)
+
 variable {T}
 
 instance partition.part_box : has_coe_to_fun (partition T) 
     := { F := λ _, T → set T, coe := λ (p : partition T), partition.box p }
 
-definition partition.dit (P : partition T) (s t : T) : Prop := t ∉ P s
+-- Note the switch of P and Q.
+def refined_by (P Q : partition T) : Prop := forall x, Q.box x ≤ P.box x  
+def partition.indit (P : partition T) (x y : T) : Prop := P x = P y
+def partition.dit (P : partition T) (x y : T) : Prop := P x ≠ P y 
 
-def refined_by (P Q : partition T) := ∀ s t, partition.dit P s t -> partition.dit Q s t
-
-instance partition_ : preorder (partition T) := 
+instance partition_refinement : preorder (partition T) := 
 { le := refined_by
-, le_refl := \l}
+, le_refl := λ _ _ _ H, H
+, le_trans := λ _ _ _ f g t _ x, f t (g t x)
+}
+
+
+
+end
